d6d22fb0a082daf7d83a860623008166ada2db48
김민식 <alstlr0404@gmail.com>
2022-01-26 10:00:36 +0900

    java 별찍기

--- src/main/java/jpabook/jpashop/Practice.java
+++ src/main/java/jpabook/jpashop/Practice.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Java 별찍기
 public class Practice {
 
     public static void main(String[] args) throws IOException {
+
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("원하는 중간 행 입력: ");
